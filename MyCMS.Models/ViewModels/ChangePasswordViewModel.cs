136f53ad98bb041a685a636afb53fc892379aaea
AliisKMC <alirezapishadast@gmail.com>
2025-09-23 16:24:12 +0330

    0701-1

---
+++ MyCMS.Models/ViewModels/ChangePasswordViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyCMS.Models.ViewModels
+{
+    public class ChangePasswordViewModel
+    {
+        [Required(ErrorMessage = "لطفا کلمه عبور فعلی را وارد کنید")]
+        [MaxLength(255)]
+        [DataType(DataType.Password)]
+        public string OldPassword { get; set; }
+
+        [Required(ErrorMessage = "لطفا کلمه عبور را وارد کنید")]
+        [MaxLength(255)]
+        [DataType(DataType.Password)]
+        //[RegularExpression("^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$",
+        // ErrorMessage = "پسوردت امن نیست")]
+        public string Password { get; set; }
+        [Required(ErrorMessage = "لطفا تکرار کلمه عبور جدید را وارد کنید")]
+        [MaxLength(255)]
+        [Compare("Password", ErrorMessage = "کلمه های عبور جدید مغایرت دارند")]
+        [DataType(DataType.Password)]
+        public string RePassword { get; set; }
+    }
+}
--- MyCMS/Areas/UserPanel/Controllers/HomeController.cs
+++ MyCMS/Areas/UserPanel/Controllers/HomeController.cs
@@ -1,13 +1,55 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MyCMS.DataAccess.Services;
+using MyCMS.Models.ViewModels;
+using MyCMS.Utilities.Security;
+using System.Security.Claims;
 
 namespace MyCMS.Areas.UserPanel.Controllers
 { 
     [Area("UserPanel")]
     public class HomeController : Controller
     {
+        private readonly IUserService _UserService;
+        public HomeController(IUserService userService)
+        {
+            _UserService = userService;
+        }
+
         public IActionResult Index()
         {
             return View();
         }
+
+        #region ChangePassword
+        public IActionResult ChangePassword() 
+        { 
+            return View();
+        }
+        [HttpPost]
+        public IActionResult ChangePassword(ChangePasswordViewModel vmPasswordViewModel)
+        {
+            if (!ModelState.IsValid) 
+                return View(vmPasswordViewModel);
+
+            string strCurrentUser=User.FindFirstValue("UserId");
+            int intUserID = 0;
+
+            if (strCurrentUser != null)
+                intUserID = int.Parse(strCurrentUser);
+            else
+                throw new Exception("User Not Fount ...!");
+            var user=_UserService.GetById(intUserID);
+            if (!PasswordHasher.VerifyHashedPassword(user.Password,vmPasswordViewModel.OldPassword))
+            {
+                ModelState.AddModelError("", "کلمه عبور فعلی صحیح نمی باشد");
+                return View(vmPasswordViewModel);
+            }
+
+            user.Password = PasswordHasher.HashPassword(vmPasswordViewModel.Password);
+            _UserService.Update(user);
+
+            return Redirect("/Account/Logout");
+        }
+        #endregion
     }
 }
