781931262a262cd4f1e66b26034e6bb6dbd0d8f3
AliisKMC <alirezapishadast@gmail.com>
2025-09-25 08:41:02 +0330

    0703-1

--- MyCMS.Models/Model/BaseEntity.cs
+++ MyCMS.Models/Model/BaseEntity.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
@@ -11,8 +12,11 @@ namespace MyCMS.Models.Model
     {
         [Key]
         public int Id { get; set; }
+        [DisplayName("تاریخ ایجاد")]
         public DateTime CreateDate { get; set; }
+        [DisplayName("تاریخ تغییر")]
         public DateTime? ModifiedDate { get; set; }
+        [DisplayName("حذف شده؟")]
         public bool IsDelete { get; set; }
 
     }
--- MyCMS.Models/Model/User.cs
+++ MyCMS.Models/Model/User.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
@@ -9,18 +10,22 @@ namespace MyCMS.Models.Model
 {
     public class User:BaseEntity
     {
+        [DisplayName("نام کاربری")]
         [Required(ErrorMessage = "لطفا نام را وارد کنید")]
         [MaxLength(255)]
         public string UserName { get; set; }
 
+        [DisplayName("رمز عبور")]
         [Required(ErrorMessage = "لطفا کلمه عبور را وارد کنید")]
         [MaxLength(255)]
         public string Password { get; set; }
 
+        [DisplayName("ایمیل")]
         [Required(ErrorMessage = "لطفا ایمیل را وارد کنید")]
         [MaxLength(255)]
         [EmailAddress(ErrorMessage = "ایمیل وارد شده معتبر نمی باشد")]
         public string Email { get; set; }
+        [DisplayName("ادمین است؟")]
         public bool IsAdmin { get; set; }
 
     }
